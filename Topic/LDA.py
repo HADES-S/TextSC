82640a4df890aa31e8cfc37bd9351f301089e266
施裕豪 <759936723@qq.com>
2016-08-30 14:49:54 +0800

    创建主题模型的初始化程序
    Signed-off-by: 施裕豪 <759936723@qq.com>

---
+++ Topic/LDA.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#encoding=utf-8
+#Script Name :LDA.py
+#Author : Simon Shi
+#created : 2016.8.30
+#Version : 1.0
+#Description : 对语料库中的数据进行主题模型处理
+
+import gensim,os
---
+++ Topic/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#encoding=utf-8
+#Script Name :
+#Author : Simon Shi
+#created : 
+#Version : 1.0
+#Description : 处理自己的语料库
